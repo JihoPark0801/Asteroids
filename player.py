25cb4979e506d7e01d8f3eb9ae2a64ebc3a380f9
JihoPark0801 <jihopark2002@gmail.com>
2025-09-15 21:17:39 -0700

    added player movement functionality

--- player.py
+++ player.py
@@ -1,5 +1,5 @@
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS
+from constants import *
 import pygame
 
 class Player(CircleShape):
@@ -17,5 +17,28 @@ class Player(CircleShape):
     
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
+        return
+    
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+        return
+    
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            # ?
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            # ?
+            self.rotate(dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
+
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
         return
     
